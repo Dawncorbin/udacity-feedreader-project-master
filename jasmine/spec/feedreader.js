676504aadd7ce84b69de5d38accd9be0536375a8
Dawn Corbin <imadesigner@hotmail.com>
2018-08-13 20:42:05 -0700

    Initial Entries testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,20 +75,27 @@ $(function() {
 
             menu.click();
             expect(body.classList.contains('menu-hidden')).toBe(false);
-            expect(body.classList.contains('menu-hidden')).not.toBe(true);
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
         });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
 
-
-
-        /* TODO: Write a test that ensures when the loadFeed
+      /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+       beforeEach(function(done) {
+            loadFeed(0, done);
+       });
+
+          it('completes its work', function() {
+              const feed = document.querySelector('.feed');
+              expect(feed.children.length > 0).toBe(true);
+          });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
@@ -96,5 +103,9 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            loadFeed(0);
+            loadFeed(1, done);
+        });
     });
 }());
