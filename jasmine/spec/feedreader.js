16de6d19575020768b1cf894fc7ec4b3582e73bd
Dawn Corbin <imadesigner@hotmail.com>
2018-08-13 19:39:34 -0700

    Added describe to test suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -69,8 +69,14 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('toggles on and off', function() {
+              expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
       });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -78,11 +84,12 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
